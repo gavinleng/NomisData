0d2cc5cbb82a23bff00328ee521506d59a5fa95c
Gavin <gleng.email@gmail.com>
2015-11-05 09:08:45 +0000

    add pkey

--- NomisData_downloader.py
+++ NomisData_downloader.py
@@ -7,7 +7,7 @@ import pandas as pd
 import argparse
 import json
 import datetime
-
+import hashlib
 
 # url = "https://www.nomisweb.co.uk/api/v01/dataset/NM_18_1.data.csv?geography=1946157199...1946157245&date=latest&age=MAKE|Aged%2016-24|1;2&duration=MAKE|Up%20to%206%20months|1...7,MAKE|Over%206%20months%20and%20up%20to%20a%20year|8;9,MAKE|Over%201%20year|10...16&sex=5,6&measures=20100,20206&select=geography_code,geography_name,sex_name,age_name,duration_name,measures_name,obs_value,date"
 # output_path = "tempYouthUnemployment.csv"
@@ -16,27 +16,22 @@ import datetime
 def download(url, outPath):
     dName = outPath
 
-    try:
-        socket = urllib.request.urlopen(url)
-    except urllib.error.HTTPError as e:
-        errfile.write(str(now()) + ' csv download HTTPError is ' + str(e.code) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('csv download HTTPError = ' + str(e.code))
-    except urllib.error.URLError as e:
-        errfile.write(str(now()) + ' csv download URLError is ' + str(e.args) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('csv download URLError = ' + str(e.args))
-    except Exception:
-        print('csv file download error')
-        import traceback
-        errfile.write(str(now()) + ' generic exception: ' + str(traceback.format_exc()) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('generic exception: ' + traceback.format_exc())
+    # open url
+    socket = openurl(url)
 
     # load this csv file
     logfile.write(str(now()) + ' csv file loading\n')
     print('csv file loading------')
     df = pd.read_csv(socket, dtype='unicode')
+
+    # create primary key by md5 for each row
+    logfile.write(str(now()) + ' create primary key\n')
+    print('create primary key------')
+    col = df.columns.tolist() + ['pkey']
+    keyCol = [0, 2, 3, 4, 5, 7]
+    df[col[-1]] = fpkey(df, col, keyCol)
+    logfile.write(str(now()) + ' create primary key end\n')
+    print('create primary key end------')
 
     # save csv file
     logfile.write(str(now()) + ' writing to file\n')
@@ -46,6 +41,36 @@ def download(url, outPath):
     print('Requested data has been extracted and saved as ' + dName)
     logfile.write(str(now()) + ' finished\n')
     print("finished")
+
+def openurl(url):
+    try:
+        socket = urllib.request.urlopen(url)
+        return socket
+    except urllib.error.HTTPError as e:
+        errfile.write(str(now()) + ' file download HTTPError is ' + str(e.code) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
+        sys.exit('file download HTTPError = ' + str(e.code))
+    except urllib.error.URLError as e:
+        errfile.write(str(now()) + ' file download URLError is ' + str(e.args) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
+        sys.exit('file download URLError = ' + str(e.args))
+    except Exception:
+        print('file download error')
+        import traceback
+        errfile.write(str(now()) + ' generic exception: ' + str(traceback.format_exc()) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
+        sys.exit('generic exception: ' + traceback.format_exc())
+
+def fpkey(data, col, keyCol):
+    mystring = ''
+    pkey = []
+    for i in range(len(data[col[0]])):
+        for j in keyCol:
+            mystring += str(data[col[j]][i])
+        mymd5 = hashlib.md5(mystring.encode()).hexdigest()
+        pkey.append(mymd5)
+
+    return pkey
 
 def now():
     return datetime.datetime.now()
